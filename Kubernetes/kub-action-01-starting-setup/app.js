155419f70ea0d8a7275017f154c715f34ac1f327
OscarRecinosxd <oscarrecinos2019@gmail.com>
2023-06-28 01:05:56 -0600

    finishing section 12

--- Kubernetes/kub-action-01-starting-setup/app.js
+++ Kubernetes/kub-action-01-starting-setup/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!!!!!!</h1>
+    <h1>!!!!Hello from this NodeJS app!!!!!!</h1>
     <h2>Hola</h2>
     <p>Try sending a request to /error and see what happens</p>
     <h2>This is new<h2>
